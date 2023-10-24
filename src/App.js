837481f1d9f705c9b7d3012383d44b4e2b7cd8ea
Sean Connelly <sean@seanconnelly.me>
2023-10-24 14:50:18 -0400

    fixed history bug and key error

--- src/App.js
+++ src/App.js
@@ -69,12 +69,12 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description - 'Go to move #' + move; 
+      description = 'Go to move #' + move; 
     } else {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
